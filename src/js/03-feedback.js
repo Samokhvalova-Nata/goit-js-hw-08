61779b7d5fc9d14606485c30ffaa86bdbb4af47a
Nataliia Samokhvalova <samohvalova520@gmail.com>
2023-03-06 16:27:08 +0200

    Update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,18 +6,10 @@ const form = document.querySelector('.feedback-form');
 const input = form.querySelector('input');
 const textarea = form.querySelector('textarea');
 
-const formData = {};
+populateInputs();
 
 form.addEventListener('input', throttle(hahdleInputChange, 500));
 form.addEventListener('submit', handleFormSubmit);
-
-populateInputs();
-
-function hahdleInputChange(evt) {
-    formData[evt.target.name] = evt.target.value;
-
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
@@ -25,19 +17,26 @@ function handleFormSubmit(evt) {
     const { elements: { email, message } } = evt.target;
     if (email.value === '' || message.value === '') {
         return alert('Всі поля повинні бути заповнені');
-    } 
-
-    evt.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
+    }
+    const formData = { email: email.value, message: message.value };
     console.log(formData);
+    evt.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function hahdleInputChange(evt) {
+    let filledInputs = localStorage.getItem(STORAGE_KEY);
+    filledInputs = filledInputs ? JSON.parse(filledInputs) : {};
+    filledInputs[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(filledInputs));
 }
 
 function populateInputs() {
-    const savedData = localStorage.getItem(STORAGE_KEY);
-    const parsedData = JSON.parse(savedData);
-
-    if (parsedData) {
-        input.value = parsedData.email
-        textarea.value = parsedData.message
+    let savedData = localStorage.getItem(STORAGE_KEY);
+    if (savedData) {
+        savedData = JSON.parse(savedData);
+        Object.entries(savedData).forEach(([name, value]) => {
+        form.elements[name].value = value;
+        })
     }
 }
